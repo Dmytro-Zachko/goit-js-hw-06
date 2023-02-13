cb6035b2fdc72769ece0ccf6c03005c3dd8e02f1
Bestis21 <zaichkodmytro2004@gmail.com>
2023-02-13 11:23:30 +0100

    9/10

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
-
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const FormEl = event.currentTarget.elements
+    const email = FormEl.email.value
+    const password = FormEl.password.value
+    const FormObject = {
+        email,
+        password,
+    }
+    if (!email && !password) {
+        alert('Всі поля повинні бути заповнені')
+        }
+    console.log(FormObject);
+        formRef.reset()
+})
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
+const bodyRef = document.querySelector('body')
+const btnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+btnRef.addEventListener('click', () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor()
+})
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
